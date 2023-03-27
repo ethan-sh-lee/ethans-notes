d0111677f414ba9c5d156fe7832995d1b6e9b4cd
sunho-lee <sh112123@naver.com>
2023-03-27 11:15:19 +0900

    font 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Inter } from "next/font/google";
 import { compareDesc } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import PostCard from "./components/PostCard";
 
 //메인 화면
 //포스트 카드와 포스트 리스트로 블로그 글을 탐색할 수 있도록 함
-const inter = Inter({ subsets: ["latin"] });
 
 async function getData() {
   const posts = allPosts.sort((a, b) => {
