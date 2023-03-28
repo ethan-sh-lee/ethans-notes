ce3e0b608b5590732c8b439edb0791973c895418
sunho-lee <sh112123@naver.com>
2023-03-28 11:28:52 +0900

    add nextjs config for giscus env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const { withContentlayer } = require("next-contentlayer");
 
 module.exports = withContentlayer({
+  env: {
+    GISCUS_REPO_USERNAME: process.env.GISCUS_REPO_USERNAME,
+    GISCUS_REPO_NAME: process.env.GISCUS_REPO_NAME,
+    GISCUS_REPO_ID: process.env.GISCUS_REPO_ID,
+    GISCUS_CATEGORY_ID: process.env.GISCUS_CATEGORY_ID,
+  },
   experimental: {
     appDir: true,
   },
