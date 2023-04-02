69f4ccaee6a98e1ca4a7760e413daf5d3bc6c144
sunho-lee <sh112123@naver.com>
2023-04-02 16:53:17 +0900

    cpp 이미지 추가

--- src/app/components/TagLang.tsx
+++ src/app/components/TagLang.tsx
@@ -1,9 +1,15 @@
 import { DailyLog } from "contentlayer/generated";
+import Image from "next/image";
+import cppLogo from "../../../public/image/cpp_logo.png";
 
 export default function Tag({ value }: { value: DailyLog["lang"] }) {
-  return (
-    <span className="inline-flex items-center px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded-full text-xs font-semibold text-gray-600">
-      <span className="ml-1">{value}</span>
-    </span>
-  );
+  if (value === "cpp") {
+    return <Image src={cppLogo} height={20} width={20} alt="cpp" />;
+  } else {
+    return (
+      <span className="inline-flex items-center px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded-full text-xs font-semibold text-gray-600">
+        <span className="ml-1">{value}</span>
+      </span>
+    );
+  }
 }
