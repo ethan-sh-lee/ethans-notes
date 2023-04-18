cd89afc8a29584ab6c3b874af52bec8cc40ab112
sunho-lee <sh112123@naver.com>
2023-04-18 15:41:49 +0900

    페이지 레이아웃 패딩 바텀 추가

--- src/app/components/layout/PageLayout.tsx
+++ src/app/components/layout/PageLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export const PageLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="pt-4 mx-auto max-w-4xl">
+        <div className="py-4 mx-auto max-w-4xl">
             {children}
         </div>
     )
