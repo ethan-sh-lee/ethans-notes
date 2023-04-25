9dd11bf8bea81f13b03ca97843f12045727ba7fa
sunho-lee <sh112123@naver.com>
2023-04-25 16:07:23 +0900

    post card link 리팩토링

--- src/app/components/card/PostCard.tsx
+++ src/app/components/card/PostCard.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { format, parseISO } from "date-fns";
 import { Post } from "contentlayer/generated";
 
@@ -8,11 +7,9 @@ export default function PostCard(post: Post) {
       <time dateTime={post.publishedAt} className="block text-sm text-slate-600">
         {format(parseISO(post.publishedAt), "LLLL d, yyyy")}
       </time>
-      <h2 className="text-lg">
-        <Link className="text-3xl font-bold" href={post.url.replace("/posts", "")}>
-          {post.title}
-        </Link>
-      </h2>
+      <h2 className="text-3xl font-bold">
+        {post.title}
+      </h2 >
     </>
   );
 }
