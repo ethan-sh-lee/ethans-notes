ef64a7c7e647a05f8b72e5c6804ce89b1ad304ec
sunho-lee <sh112123@naver.com>
2023-04-28 16:40:01 +0900

    fix bug

--- src/app/components/layout/SimplePostLayout.tsx
+++ src/app/components/layout/SimplePostLayout.tsx
@@ -1,28 +1,28 @@
-import { Post } from "contentlayer/generated"
+import { Post } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { format, parseISO } from "date-fns";
-import Comments from "@/app/components/Comments";
+import { Comments } from "@/app/components/Comments";
 import "@/styles/prism-darcula.css";
 import "@/styles/prism.css";
 
 // Post 타입 컨텐츠
 export const SimplePostLayout = ({ post }: { post: Post }) => {
-    const MDXContent = useMDXComponent(post?.body?.code);
-    return (
-        <div className="px-8 mx-auto xl:px-5 max-w-screen-lg py-5 lg:py-8 prose">
-            <title>{post.title}</title>
-            <article>
-                <div>
-                    <h1 className="text-3xl font-bold">{post.title}</h1>
-                    <time dateTime={post.publishedAt}>
-                        {format(parseISO(post.publishedAt), "LLLL d, yyyy")}
-                    </time>
-                </div>
-
-                <MDXContent />
-                <div className="pt-8" />
-                <Comments />
-            </article>
+  const MDXContent = useMDXComponent(post?.body?.code);
+  return (
+    <div className="px-8 mx-auto xl:px-5 max-w-screen-lg py-5 lg:py-8 prose">
+      <title>{post.title}</title>
+      <article>
+        <div>
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+          <time dateTime={post.publishedAt}>
+            {format(parseISO(post.publishedAt), "LLLL d, yyyy")}
+          </time>
         </div>
-    )
-}
+
+        <MDXContent />
+        <div className="pt-8" />
+        <Comments />
+      </article>
+    </div>
+  );
+};
