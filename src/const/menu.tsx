889284056e592b8106327c5165d763e219ab5acc
sunho-lee <sh112123@naver.com>
2023-05-02 13:28:12 +0900

    메뉴 수정

--- src/const/menu.tsx
+++ src/const/menu.tsx
@@ -9,36 +9,27 @@ export const noteCategories: Category[] = [
   {
     title: "문제풀이를 위한 스킬들",
     description: "알고리즘 문제 풀이를 위한 구현 스킬을 정리합니다.",
-    href: "/notes/skills",
+    href: "/notes/algorithm-skills",
     tags: ["cpp"],
   },
   {
-    title: "코틀린과 스프링부트로 백엔드 개발하기",
-    description: "코틀린, 스프링부트, 웹플럭스로 백엔드 구현하기",
-    href: "/notes/spring-kotlin",
-    tags: [
-      "Kotlin",
-      "Spring",
-      "MSA",
-      "Reactive",
-      "Mysql",
-      "MongoDB",
-      "Kafka",
-      "Redis",
-      "Monitoring",
-      "Tracing",
-    ],
+    title: "Javascript 문법 및 활용",
+    description:
+      "웹 서비스에서 가장 광범위하게 사용되는 Javascript를 정리한다.",
+    href: "/notes/javascript",
+    tags: ["문법", "DOM조작"],
   },
   {
-    title: "언어 문법 빠르게 정리하기",
-    description: "언어 문법 위주로 간단하게 정리",
-    href: "/notes/syntax",
-    tags: ["제어문", "반복문", "객체지향", "I/O"],
+    title: "NextJs13로 정적 블로그 만들기",
+    description:
+      "NextJs13, Contentlayer, TailwindCSS, Typescript로 마크다운 기반 정적 블로그 만들기",
+    href: "/notes/make-blog",
+    tags: ["NextJs", "Contentlayer", "TailwindCSS", "Typescript", "Markdown"],
   },
   {
     title: "좋은 코드란 무엇인가",
     description: "유지보수하기 좋고 깨지지 않는 좋은 코드 작성을 위한 것들",
     href: "/notes/clean-code",
-    tags: ["리팩토링", "클린코드", "객체지향", "함수형"],
+    tags: ["유지보수성", "버그방지", "가독성"],
   },
 ];
