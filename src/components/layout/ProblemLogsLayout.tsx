fced01c4341c5ae6635f93fbd5b1531ddcbf8bd0
sunho-lee <sh112123@naver.com>
2023-05-15 13:39:34 +0900

    p 태그 리팩토링

--- src/components/layout/ProblemLogsLayout.tsx
+++ src/components/layout/ProblemLogsLayout.tsx
@@ -1,6 +1,7 @@
 import { Log } from "contentlayer/generated";
 import DailyLogCard from "@/components/card/DailyLogCard";
 import { ArrowButton } from "@/components/button/ArrowButton";
+import { H2 } from "../typo/heading";
 
 export const ProblemLogsLayout = ({
   logs,
@@ -13,9 +14,10 @@ export const ProblemLogsLayout = ({
 }) => {
   return (
     <div className="mx-auto max-w-4xl">
-      <h2 className="text-4xl font-extrabold dark:text-white">{title}</h2>
+      <H2>{title}</H2>
+      <div className="mb-4" />
       <ArrowButton link={link} title="문제 보러가기" />
-      <div className="flex-1 bg-neutral-50 dark:bg-gray-800 rounded-lg my-2 shadow-xl p-2 md:p-4">
+      <div className="flex-1 bg-neutral-50 dark:bg-gray-800 rounded-lg my-2 shadow p-2 md:p-4">
         <div className="relative px-2 md:px-4">
           <div className="absolute h-full border border-dashed border-opacity-20 border-secondary"></div>
           {logs.map((log, idx) => (
