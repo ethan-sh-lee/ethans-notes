ca5f0bc8db4e9f3c1f2d785407a490a721cebe44
sunho-lee <sh112123@naver.com>
2024-07-10 15:16:42 +0900

    [debug] 중괄호 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,7 +85,7 @@ module.exports = {
     },
   },
   plugins: [
-    [require("@tailwindcss/typography")],
+    require("@tailwindcss/typography"),
     require("tailwindcss-animate"), //shadcn
   ],
 };
